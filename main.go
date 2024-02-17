432048a7918bcb873c0c92542488f15ca1843a94
Phi <phihdn@gmail.com>
2024-02-17 15:03:00 -0400

    add catch command

--- main.go
+++ main.go
@@ -10,11 +10,13 @@ type config struct {
 	pokeapiClient       pokeapi.Client
 	nextLocationAreaURL *string
 	prevLocationAreaURL *string
+	caughtPokemon       map[string]pokeapi.Pokemon
 }
 
 func main() {
 	cfg := config{
 		pokeapiClient: pokeapi.NewClient(5 * time.Minute),
+		caughtPokemon: make(map[string]pokeapi.Pokemon),
 	}
 	startRepl(&cfg)
 }
